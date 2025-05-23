7b680004afa60284757179d63b59afeb6fd8203b
iantsa446 <iantsafy@gmail.com>
2025-05-23 09:32:18 +0200

    Update gpt_bot.py

--- gpt_bot.py
+++ gpt_bot.py
@@ -15,7 +15,7 @@ def welcome(message):
     if chat_id not in confirmed_users:
         bot.send_message(
             chat_id,
-            "👋 Faly miarahaba anao!
+            "Faly miarahaba anao!
 
 "
             "🔒 Ampidiro azafady ny teny miafina: **iantsafybot12**"
